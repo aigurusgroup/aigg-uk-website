e57ff01fd46b5b89ecb3a3b8ede8fea91972eb4c
aigurusgroup <geouffrey@aigurusgroup.com>
2026-04-16 11:38:27 +0200

    Update vite.config.ts to include base property

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/aigg-uk-website/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
